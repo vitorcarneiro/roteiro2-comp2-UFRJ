b62d308bcd571be1c4e28ac12e8c47d52231c738
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2021-12-09 10:06:33 -0300

    created aula0201.h

---
+++ aula0201.h
@@ -0,0 +1,28 @@
+/* 
+Universidade Federal do Rio de Janeiro
+Escola Politecnica
+Departamento de Eletronica e de Computacao
+EEL270 - Computacao II - Turma 2021/2
+Prof. Marcelo Luiz Drumond Lanza
+Autor: Vitor Carneiro Silva de Oliveira
+Descricao: Prototipo da funcao CalcularFibonacci
+
+$Author$
+$Date$
+$Log$
+
+*/
+
+#ifndef _AULA0201_
+#define _AULA0201_  "@(#)aula0201.h $Revision$"
+
+typedef unsigned short us;
+
+typedef unsigned long long ull;
+
+ull
+CalcularTermoSerieFibonacci (us);
+
+#endif
+
+/* $RCSfile */
